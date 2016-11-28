4d403f4c0b43cbe954533b5c9b2260fd119497c3
Jucong He <hejucong1995@gmail.com>
2016-11-27 20:54:00 -0500

    static

--- server/src/server.js
+++ server/src/server.js
@@ -5,24 +5,11 @@ var reverseString = utilModule.reverseString;
 var express = require('express');
 //Creates an Express server.
 var app = express();
-app.use(bodyParser.text());
 
-// Defines what happens when it receives the 'GET /' request
-app.get('/',function(req,res) {
-  res.send('Hello World!');
-});
+app.use(bodyParser.text());
+app.use(express.static('../client/build'));
 
 //Starts the Server on port 3000!
 app.listen(3000,function() {
   console.log('Example app listening on port 3000');
 });
-
-// Handle Post /reverse [data]
-app.post('/reverse',function(req,res) {
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    res.status(400).end();
-  }
-})
